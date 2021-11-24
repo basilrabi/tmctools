16d10376f89b7c0a731805b6274b337b5dadf961
Basil Eric Rabi <ericbasil.rabi@gmail.com>
2021-11-24 09:33:47 +0800

    add option to use double qoutes to enclose query

--- R/psql.R
+++ R/psql.R
@@ -7,13 +7,22 @@
 #' @param db_name database
 #' @param query a single query to be executed
 #' @param ignore.stdout see \link{system}
+#' @param use_single_quoute use single quotes to enclose query string. If false,
+#'   double quotes are used instead.
 #' @return NULL if success
 #' @export psql
-psql <- function(db_host, db_user, db_name, query, ignore.stdout = TRUE) {
-  cmd <- paste0("psql -h ", db_host,
-                " -U ", db_user,
-                " -d ", db_name,
-                " -c '", query, "'")
+psql <- function(db_host,
+                 db_user,
+                 db_name,
+                 query,
+                 ignore.stdout = TRUE,
+                 use_single_quoute = TRUE) {
+  cmd <- paste0("psql -h ", db_host, " -U ", db_user, " -d ", db_name, " -c")
+  if (use_single_quoute) {
+    cmd <- paste0(cmd, "'", query, "'")
+  } else {
+    cmd <- paste0(cmd, '"', query, '"')
+  }
   if (system(cmd, ignore.stdout = ignore.stdout) != 0)
     stop("PSQL command error.")
 
